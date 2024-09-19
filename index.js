4d62b209c49e02ea8da9dca7f02bb902435b05e4
codeswith <deshpanderoshan00@gmail.com>
2024-09-19 11:13:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let bgContEl = document.getElementById("bgCont");
+
+let bgTextEl = document.getElementById("bgCText");
+
+function changeColor1(){
+    bgContEl.style.backgroundColor = "#e0e0e0";
+    bgTextEl.textContent = "#e0e0e0";
+    bgTextEl.style.color = "#e0e0e0";
+}
+function changeColor2() {
+  bgContEl.style.backgroundColor = "#6fcf97";
+  bgTextEl.textContent = "#6fcf97";
+  bgTextEl.style.color = "#6fcf97";
+}
+function changeColor3() {
+  bgContEl.style.backgroundColor = "#56ccf2";
+  bgTextEl.textContent = "#56ccf2";
+  bgTextEl.style.color = "#56ccf2";
+}
+function changeColor4() {
+  bgContEl.style.backgroundColor = "#bb6bd9";
+  bgTextEl.textContent = "#bb6bd9";
+  bgTextEl.style.color = "#bb6bd9";
+}
